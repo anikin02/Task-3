844b7f9711039dcd1c46ef598b319951a351fb5b
Danil Anikin <mart4r.clearlist@gmail.com>
2025-01-23 12:40:29 +1000

    Initial Commit

---
+++ Task-3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Task-3
+//
+//  Created by Данил Аникин on 23.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Task-3/Task_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Task_3App.swift
+//  Task-3
+//
+//  Created by Данил Аникин on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
